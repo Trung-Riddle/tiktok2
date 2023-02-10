7a7faf45970d4cd5b8c567f2f059cfbdb63a399c
Trung-Riddle <nguyendinhtrung06052001@gmail.com>
2023-02-10 14:24:43 +0700

    search header focus

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,14 +1,18 @@
-
+import classNames from "classnames/bind";
+import styles from "./DefaultLayout.module.scss"
 import Header from "~/components/Layout/components/Header";
 import Sidebar from "./Sidebar";
 
+
+var cx = classNames.bind(styles)    
+
 function DefaultLayout({children}) {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="container">
+            <div className={cx('container')}>
                 <Sidebar />
-                <div className="contents">
+                <div className={cx('contents')}>
                     {children}
                 </div>
             </div>
